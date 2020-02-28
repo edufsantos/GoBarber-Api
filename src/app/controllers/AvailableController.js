c30fc636bd17db06f9590208b2d83c599de41827
edufsantos <eduardosantosifms@gmail.com>
2020-02-28 15:31:25 -0400

    first api-appointments

---
+++ src/app/controllers/AvailableController.js
@@ -0,0 +1,71 @@
+import { Op } from 'sequelize';
+import {
+  startOfDay,
+  endOfDay,
+  format,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isAfter,
+} from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date!' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    // horarios disponiveis do provedor
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      // cirando uma desistruturação informando através do split que tudo que vier antes dos : é a hora e o oque vem dps é minuto
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy:MM:dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
+  }
+}
+
+export default new AvailableController();
